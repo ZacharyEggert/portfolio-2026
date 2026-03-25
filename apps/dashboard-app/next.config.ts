ff0b2f14139b02b36c3571614817d222ea05443d
ZacharyEggert <eggert.zachary@gmail.com>
2026-03-24 20:40:06 -0700

    wipe starter page

--- apps/dashboard-app/next.config.ts
+++ apps/dashboard-app/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+	turbopack: {
+		root: join(__dirname, "../.."),
+	}
 };
 
 export default nextConfig;
